986eb08a8100106e3925851a143f6cd2c8b65f1e
Nico Westerbeck <Nico.Westerbeck@mailbox.tu-dresden.de>
2017-06-02 12:18:54 +0300

    Added gpio code

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 #!flask/bin/python
 from flask import Flask, Response
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BOARD)
+pin = 8
+GPIO.setup(pin, GPIO.OUT)
+GPIO.output(pin, GPIO.LOW)
+
 
 app = Flask(__name__)
 
 @app.route('/backend')
 def backend():
-    print("halasdakshdlaks")
-    return "Hello, World!"
+    print("Pulling up")
+    GPIO.output(pin, GPIO.HIGH)
+    sleep(1)
+    GPIO.output(pin, GPIO.LOW)
+    return "Successfully triggered"
 
 @app.route('/')
 def index():
